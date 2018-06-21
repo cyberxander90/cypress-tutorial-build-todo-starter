cd7ea8dc79b72e7463eac51e7b78b6b0f14b3551
Alexander Leyva <cyberxander90@gmail.com>
2018-06-20 22:47:44 -0300

    test input is focused on load, and type text is set into the input

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,5 +1,17 @@
 describe('Input form', () => {
   it('focuses input on load', () => {
     cy.visit('http://localhost:3030');
+
+    cy.focused()
+      .should('have.class', 'new-todo');
+  })
+
+  it.only('inputs texts', () => {
+    const typedText = 'Buy Milk';
+    cy.visit('http://localhost:3030');
+
+    cy.get('.new-todo')
+      .type(typedText)
+      .should('have.value', typedText);
   })
 })
